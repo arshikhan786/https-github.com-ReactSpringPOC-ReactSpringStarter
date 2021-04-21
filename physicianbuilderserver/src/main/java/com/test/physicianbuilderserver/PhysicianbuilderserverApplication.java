340cd1fd9ec79e6ed19e57c8f26089826cc81f6d
swethap106 <swprabhakaran@gmail.com>
2021-04-21 18:55:10 +0530

    initialcode

---
+++ physicianbuilderserver/src/main/java/com/test/physicianbuilderserver/PhysicianbuilderserverApplication.java
@@ -0,0 +1,14 @@
+package com.test.physicianbuilderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicianbuilderserverApplication {
+
+	public static void main(String[] args) {
+		System.out.print("app run");
+		SpringApplication.run(PhysicianbuilderserverApplication.class, args);
+	}
+
+}
