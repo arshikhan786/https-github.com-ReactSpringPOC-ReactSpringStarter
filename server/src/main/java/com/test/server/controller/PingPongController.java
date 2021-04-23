05a03cde182bd7e0d87997b2ef60438f1db2cf13
swethap106 <swprabhakaran@gmail.com>
2021-04-23 14:40:42 +0530

    ping pong

---
+++ server/src/main/java/com/test/server/controller/PingPongController.java
@@ -0,0 +1,20 @@
+package com.test.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PingPongController {
+	{
+		System.out.println("Ping Pong");
+		
+	}
+	
+	@GetMapping("/ping")
+	public String pingPong()
+	{
+		return "pong";
+	}
+}
