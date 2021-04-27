c1073c16bf44c90577a34c33db256fa5ef431dc0
swethap106 <swprabhakaran@gmail.com>
2021-04-27 13:20:21 +0530

    filter

---
+++ server/src/main/java/com/test/server/config/SecurityFilter.java
@@ -0,0 +1,23 @@
+package com.test.server.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+
+public class SecurityFilter extends GenericFilterBean {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("filter class");
+		 chain.doFilter(request, response);
+	}
+
+}
